1cdeb810b972f58da11c35b0e6c83ea1c3f84399
Paramveer Singh <paramveer7267@gmail.com>
2025-07-17 06:56:04 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,18 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Main from './components/Main'
-import Skills from './components/Skills'
 import Header from './components/Header'
-import Qualifications from './components/Qualifications';
-import Exp from './components/Exp'
+import Home from './components/Home'
+
 
 function App() {
 
   return (
     <>
-    <Exp />
-      {/* <Header /> */}
-      {/* <Main />
-      <Skills />
-      <Qualifications />     */}
-      </>
+    <Header />
+    <Home>  
+    </Home>
+  </>
   )
 }
 
@@ -26,30 +22,3 @@ export default App
 
 
 
-// import logo from './logo.svg';
-// import './App.css';
-// import React from 'react';
-// import Navbar from './components/Navbar';
-// import About from './components/About';
-// import Skills from './components/Skills';
-// import Projects from './components/Projects';
-// import Contact from './components/Contact';
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-// import Header from './components/Header';
-// import Qualifications from './components/Qualifications';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar />
-//       {/* <About /> */}
-//       <Header />
-//       <Skills />
-//       <Qualifications />
-//       {/* <Projects />
-//       <Contact /> */}
-//     </div>
-//   );
-// }
-
-// export default App;
